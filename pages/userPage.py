07ad95391f8ba949bbd5382f7418a32208e4e284
wasimafser <wasim.afser@gmail.com>
2019-01-27 14:53:57 +0530

    Added Navigation bar

--- pages/userPage.py
+++ pages/userPage.py
@@ -22,7 +22,7 @@ from kivy.properties import NumericProperty
 from pages.navigationdrawer import NavigationDrawer
 
 from db import getInfo, usersListManip
-from pages import Calendar, table
+from pages import Calendar, table, navigationdrawer
 
 #logoutButton = Button()
 id = ''
@@ -44,127 +44,166 @@ Builder.load_string("""
 #:import InfoTab pages.infoPopup
 #:import NavigationDrawer pages.navigationdrawer
 <UserPage>:
-    canvas.before:
-        Color:
-            rgba: (189/255, 189/255, 189/255, 1)
-        Rectangle:
-            pos: self.pos
-            size: self.size
-    FloatLayout:
-        size: (Window.width, Window.height)
-        UserLabel:
-            id: userinfo
+    NavigationDrawer:
+        id: nav
+        anim_type: 'fade_in'
+        opening_transition: 'in_quint'
+        closing_transition: 'out_cubic'
         BoxLayout:
-            orientation: 'horizontal'
-            padding: (5, 0, 5, 5)
-            spacing: 5
-            size_hint_y: 0.90
+            orientation: 'vertical'
+            spacing: 2
+            canvas.before:
+                Color:
+                    rgba: (1, 1, 1, 1)
+                Rectangle:
+                    pos: self.pos
+                    size: self.size
             BoxLayout:
-                id: calendar_and_totalinfo
-                orientation: 'vertical'
+                orientation: 'horizontal'
+                size_hint_y: 0.08
+
+                # nav-close button
+                Button:
+                    size_hint_x: 0.40
+                    background_normal: 'icons/nav_close.png'
+                    background_down: 'icons/nav_close.png'
+                    on_release: nav.toggle_state()
+                Label:
+                    text: 'EXTRAS'
+                    color: (0, 0, 0, 1)
+            LeaveReqButton:
+                on_release: root.callback()
+                size_hint_y: 0.1
+            LeaveDetBtn:
+                size_hint_y: 0.1
+            Label:
+                text: ''
+        FloatLayout:
+            size: (Window.width, Window.height)
+            canvas.before:
+                Color:
+                    rgba: (189/255, 189/255, 189/255, 1)
+                Rectangle:
+                    pos: self.pos
+                    size: self.size
+
+            # nav-open button
+            Button:
+                size_hint: (0.04, 0.08)
+                pos_hint: {'top':1}
+                on_release: nav.toggle_state()
+                background_normal: 'icons/nav_open.png'
+                background_down: 'icons/nav_open_1.png'
+            UserLabel:
+                id: userinfo
+                pos_hint: {'center_y':0.98}
+            OtherLayout:
+            BoxLayout:
+                orientation: 'horizontal'
+                padding: (5, 0, 5, 5)
                 spacing: 5
-                CalendarWidgetM:
-                    size_hint:(1, 0.50)
-                    pos_hint:{'center_y':0.5, 'center_x':0.5}
-                MonPop:
-                    size_hint:(1, 0.50)
-            InfoTab:
-                size_hint:(1, 1)
-        OtherButton:
-            pos_hint: {'top':1}
-            on_release: root.callback()
-        LeaveDetBtn:
-            pos_hint: {'right': 1, 'top':1}
-        OtherLayout:
-            orientation: 'horizontal'
-            size_hint: (0.75, 0.05)
-            pos_hint: {'center_y':0.92, 'center_x':0.5}
-            spacing: 10
-            padding: (10, 0, 0, 10)
-            BoxLayout:
-                orientation: 'horizontal'
-                size_hint_x: 0.15
-                canvas.before:
-                    Color:
-                        rgba: (1, 1, 1, 1)
-                    Rectangle:
-                        pos: self.pos
-                        size: self.size
-                Button:
-                    background_color: (255, 0, 0, 0.7)
-                    size_hint_x: 0.2
-                InfoLabel:
-                    text: "Non Completed"
-                    font_name: 'fonts/GoogleSans-Bold.ttf'
-                    color: (0, 0, 0, 1)
-            BoxLayout:
-                orientation: 'horizontal'
-                size_hint_x: 0.15
-                canvas.before:
-                    Color:
-                        rgba: (1, 1, 1, 1)
-                    Rectangle:
-                        pos: self.pos
-                        size: self.size
-                Button:
-                    background_color: (0, 255, 0, 0.7)
-                    size_hint_x: 0.2
-                InfoLabel:
-                    text: "Completed"
-                    font_name: 'fonts/GoogleSans-Bold.ttf'
-                    color: (0, 0, 0, 1)
-            BoxLayout:
-                orientation: 'horizontal'
-                size_hint_x: 0.15
-                canvas.before:
-                    Color:
-                        rgba: (1, 1, 1, 1)
-                    Rectangle:
-                        pos: self.pos
-                        size: self.size
-                Button:
-                    background_color: (255, 255, 0, 0.7)
-                    size_hint_x: 0.2
-                InfoLabel:
-                    text: "Absent"
-                    font_name: 'fonts/GoogleSans-Bold.ttf'
-                    color: (0, 0, 0, 1)
-            BoxLayout:
-                orientation: 'horizontal'
-                size_hint_x: 0.15
-                canvas.before:
-                    Color:
-                        rgba: (1, 1, 1, 1)
-                    Rectangle:
-                        pos: self.pos
-                        size: self.size
-                Button:
-                    background_color: (0, 0, 255, 0.7)
-                    size_hint_x: 0.2
-                InfoLabel:
-                    text: "Holiday"
-                    font_name: 'fonts/GoogleSans-Bold.ttf'
-                    color: (0, 0, 0, 1)
-            BoxLayout:
-                orientation: 'horizontal'
-                size_hint_x: 0.15
-                canvas.before:
-                    Color:
-                        rgba: (1, 1, 1, 1)
-                    Rectangle:
-                        pos: self.pos
-                        size: self.size
-                Button:
-                    background_color: (0.5, 0.5, 0.5, 1)
-                    size_hint_x: 0.2
-                InfoLabel:
-                    text: "Sat/Sun"
-                    font_name: 'fonts/GoogleSans-Bold.ttf'
-                    color: (0, 0, 0, 1)
+                size_hint_y: 0.90
+                BoxLayout:
+                    id: calendar_and_totalinfo
+                    orientation: 'vertical'
+                    spacing: 5
+                    CalendarWidgetM:
+                        size_hint:(1, 0.50)
+                        pos_hint:{'center_y':0.5, 'center_x':0.5}
+                    MonPop:
+                        size_hint:(1, 0.50)
+                InfoTab:
+                    size_hint:(1, 1)
+
+<OtherLayout>:
+    orientation: 'horizontal'
+    size_hint: (0.75, 0.05)
+    pos_hint: {'center_y':0.92, 'center_x':0.5}
+    spacing: 10
+    padding: (10, 0, 0, 10)
+    BoxLayout:
+        orientation: 'horizontal'
+        size_hint_x: 0.15
+        canvas.before:
+            Color:
+                rgba: (1, 1, 1, 1)
+            Rectangle:
+                pos: self.pos
+                size: self.size
+        Button:
+            background_color: (255, 0, 0, 0.7)
+            size_hint_x: 0.2
+        InfoLabel:
+            text: "Non Completed"
+            font_name: 'fonts/GoogleSans-Bold.ttf'
+            color: (0, 0, 0, 1)
+    BoxLayout:
+        orientation: 'horizontal'
+        size_hint_x: 0.15
+        canvas.before:
+            Color:
+                rgba: (1, 1, 1, 1)
+            Rectangle:
+                pos: self.pos
+                size: self.size
+        Button:
+            background_color: (0, 255, 0, 0.7)
+            size_hint_x: 0.2
+        InfoLabel:
+            text: "Completed"
+            font_name: 'fonts/GoogleSans-Bold.ttf'
+            color: (0, 0, 0, 1)
+    BoxLayout:
+        orientation: 'horizontal'
+        size_hint_x: 0.15
+        canvas.before:
+            Color:
+                rgba: (1, 1, 1, 1)
+            Rectangle:
+                pos: self.pos
+                size: self.size
+        Button:
+            background_color: (255, 255, 0, 0.7)
+            size_hint_x: 0.2
+        InfoLabel:
+            text: "Absent"
+            font_name: 'fonts/GoogleSans-Bold.ttf'
+            color: (0, 0, 0, 1)
+    BoxLayout:
+        orientation: 'horizontal'
+        size_hint_x: 0.15
+        canvas.before:
+            Color:
+                rgba: (1, 1, 1, 1)
+            Rectangle:
+                pos: self.pos
+                size: self.size
+        Button:
+            background_color: (0, 0, 255, 0.7)
+            size_hint_x: 0.2
+        InfoLabel:
+            text: "Holiday"
+            font_name: 'fonts/GoogleSans-Bold.ttf'
+            color: (0, 0, 0, 1)
+    BoxLayout:
+        orientation: 'horizontal'
+        size_hint_x: 0.15
+        canvas.before:
+            Color:
+                rgba: (1, 1, 1, 1)
+            Rectangle:
+                pos: self.pos
+                size: self.size
+        Button:
+            background_color: (0.5, 0.5, 0.5, 1)
+            size_hint_x: 0.2
+        InfoLabel:
+            text: "Sat/Sun"
+            font_name: 'fonts/GoogleSans-Bold.ttf'
+            color: (0, 0, 0, 1)
 
 <UserLabel>:
     size_hint_y: 0.10
-    pos_hint: {'center_y':0.98}
     bold: True
     color: (0, 0, 0, 1)
     font_name: 'fonts/moon-bold.otf'
@@ -179,13 +218,25 @@ Builder.load_string("""
             pos: self.pos
             size: self.size
 
-<OtherButton>:
-    text: 'Extras'
-    size_hint: (0.1, 0.1)
+<LeaveReqButton>:
+    text: 'Leave Request'
+    background_color: (1, 1, 1, 0)
+    canvas.before:
+        Color:
+            rgba: (0.7, 0.7, 0.7, 1)
+        Rectangle:
+            pos: self.pos
+            size: self.size
 
 <LeaveDetBtn>:
     text: 'Leave Details'
-    size_hint: (0.1, 0.1)
+    background_color: (1, 1, 1, 0)
+    canvas.before:
+        Color:
+            rgba: (0.7, 0.7, 0.7, 1)
+        Rectangle:
+            pos: self.pos
+            size: self.size
 
 <MailInfo>:
     orientation: 'vertical'
@@ -292,7 +343,7 @@ class OtherLayout(BoxLayout):
 class InfoLabel(Label):
     pass
 
-class OtherButton(Button):
+class LeaveReqButton(Button):
     pass
 
 class Test(Label):
